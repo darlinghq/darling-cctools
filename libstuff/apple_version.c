8118180dfc58078afe816b40bc9267dfb138eb5c
Ariel Abreu <facekapow@outlook.com>
2020-06-10 12:21:59 -0400

    cctools-949.0.1

--- libstuff/apple_version.c
+++ libstuff/apple_version.c
@@ -1,1 +1,12 @@
-const char apple_version[]="cctools-" CCTOOLS_VERSION ;
+//
+//  apple_version.c
+//  stuff
+//
+//  Created by Michael Trent on 12/20/18.
+//
+
+#ifndef CURRENT_PROJECT_VERSION
+#define CURRENT_PROJECT_VERSION "cctools-localbuild"
+#endif /* CURRENT_PROJECT_VERSION */
+
+const char apple_version[] = "" CURRENT_PROJECT_VERSION;
